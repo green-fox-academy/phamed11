c58689aa72dd246d7c58556bdebca0c03a7d6b7f
Peter Antal <wrkpeter@gmail.com>
2018-08-07 01:57:03 +0200

    midnightHawk

--- week-02/day-1/src/com/greenfoxacademy/Unique.java
+++ week-02/day-1/src/com/greenfoxacademy/Unique.java
@@ -1,14 +1,38 @@
-//package com.greenfoxacademy;
-//
-//public class Unique {
-//  public static void main(String[] args) {
+package com.greenfoxacademy;
+
+
+import com.sun.tools.javac.util.ArrayUtils;
+
+import java.util.Arrays;
+
+public class Unique {
+  public static void main(String[] args) {
 ////    //  Create a function that takes a list of numbers as a parameter
 ////    //  Returns a list of numbers where every number in the list occurs only once
 ////    //  Example
 ////    //   System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
 ////    //  should print: `[1, 11, 34, 52, 61]`
-//
-//  public static int[] unique(int[] uni) {
-//
-//  }
-//}
+
+    System.out.println(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
+
+
+  }
+
+  public static String unique(int[] uni) {
+    int[] res = new int[uni.length];
+    int[] kuka = new int[uni.length];
+    Arrays.sort(uni);
+    for (int i = 0; i < uni.length - 1; i++) {
+      for (int j = 1; j < uni.length - 1; j++) {
+        if (uni[i] == uni[j]) {
+          kuka[i] = uni[i];
+        } else {
+          res[j] = uni[j];
+        }
+      }
+    }
+    return Arrays.toString(kuka);
+  }
+}
+
+
---
+++ week-02/day-2/src/SimpleReplace.java
@@ -0,0 +1,14 @@
+public class SimpleReplace {
+  public static void main(String[] args) {
+
+    String example = "In a dishwasher far far away";
+    example = example.replaceAll("dishwasher", "galaxy");
+
+//    example.replace("dishwasher", "galaxy");
+
+    System.out.println(example);
+  }
+}
+
+
+
