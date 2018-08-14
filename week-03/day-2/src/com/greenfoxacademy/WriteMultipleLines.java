90feb2e22f6b240db78a08c04c1309a126e2decf
Peter Antal <wrkpeter@gmail.com>
2018-08-14 14:59:06 +0200

    multilines finished

--- week-03/day-2/src/com/greenfoxacademy/WriteMultipleLines.java
+++ week-03/day-2/src/com/greenfoxacademy/WriteMultipleLines.java
@@ -1,16 +1,40 @@
 package com.greenfoxacademy;
 
+import javax.print.DocFlavor;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WriteMultipleLines {
   public static void main(String[] args) {
     // Create a function that takes 3 parameters: a path, a word and a number,
     // than it should write to a file.
     // The path parameter should be a string, that describes the location of the file.
     // The word parameter should be a string, that will be written to the file as lines
-    // The number paramter should describe how many lines the file should have.
+    // The number parameter should describe how many lines the file should have.
     // So if the word is "apple" and the number is 5, than it should write 5 lines
     // to the file and each line should be "apple"
     // The function should not raise any error if it could not write the file.
+    String pathTo = "apples.txt";
+    String word = "apple";
+    int num = 4;
+    multiLines(pathTo, word, num);
 
   }
 
+  public static void multiLines(String path, String word, int number) {
+    Path pathIn = Paths.get(path);
+    List<String> toWrite = new ArrayList<>();
+    for (int i = 0; i < number; i++) {
+      toWrite.add(word);
+      System.out.println();
+    }
+    try {
+      Files.write(pathIn, toWrite);
+    } catch (IOException e) {
+    }
+  }
 }
