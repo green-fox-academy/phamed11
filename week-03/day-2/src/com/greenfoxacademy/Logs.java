bdb96204d99044e9456f70931a775c1c78f51b57
Peter Antal <wrkpeter@gmail.com>
2018-08-14 17:10:32 +0200

    logs init

--- week-03/day-2/src/com/greenfoxacademy/Logs.java
+++ week-03/day-2/src/com/greenfoxacademy/Logs.java
@@ -1,11 +1,24 @@
 package com.greenfoxacademy;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Logs {
   public static void main(String[] args) {
     // Read all data from 'log.txt'.
     // Each line represents a log message from a web server
     // Write a function that returns an array with the unique IP adresses.
     // Write a function that returns the GET / POST request ratio.
+    Path path = Paths.get("log.txt");
+    try {
+      List<String> logData = Files.readAllLines(path);
+      
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
 
   }
