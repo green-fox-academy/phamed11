19bb51a2a63a25742e89bfb5d6d032128ab7dc3e
Peter Antal <wrkpeter@gmail.com>
2018-08-14 20:51:10 +0200

    unique

---
+++ week-02/day-1/src/com/greenfoxacademy/Unique.java
@@ -0,0 +1,24 @@
+package com.greenfoxacademy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Unique {
+  public static void main(String[] args) {
+    //  Create a function that takes a list of numbers as a parameter
+    //  Returns a list of numbers where every number in the list occurs only once
+
+    //  Example
+    System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+    //  should print: `[1, 11, 34, 52, 61]`
+  }
+  public static ArrayList<Integer> unique(int[] list) {
+    ArrayList<Integer> uni = new ArrayList<>();
+    for (int i = 0; i < list.length; i++) {
+      if (!uni.contains(list[i])) {
+        uni.add(list[i]);
+      }
+    }
+    return uni;
+  }
+}
