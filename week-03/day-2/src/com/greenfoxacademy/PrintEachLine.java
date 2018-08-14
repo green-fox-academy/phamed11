31afe6fcb70b7ac38dd6796e5aeb0affdd106830
Peter Antal <wrkpeter@gmail.com>
2018-08-14 11:50:51 +0200

    print each line ready

--- week-03/day-2/src/com/greenfoxacademy/PrintEachLine.java
+++ week-03/day-2/src/com/greenfoxacademy/PrintEachLine.java
@@ -1,8 +1,10 @@
 package com.greenfoxacademy;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.List;
 
 public class PrintEachLine {
   public static void main(String[] args) {
@@ -10,8 +12,15 @@ public class PrintEachLine {
     // each of the lines form the file.
     // If the program is unable to read the file (for example it does not exists),
     // then it should print an error message like: "Unable to read file: my-file.txt"
-    Path mypath = 
 
-
+    Path mypath = Paths.get("day-2.iml");
+    try {
+      List<String> allLines = Files.readAllLines(mypath);
+      for (String line : allLines) {
+        System.out.println(line);
+      }
+    } catch (IOException e) {
+      System.out.println("Unable to read file: " + mypath);
+    }
   }
 }
