7afa309a04f544295515080cbc663d814c5894f1
Peter Antal <wrkpeter@gmail.com>
2018-08-14 21:04:33 +0200

    homework init

---
+++ week-03/day-3/src/com/greenfoxacademy/Homework.java
@@ -0,0 +1,54 @@
+package com.greenfoxacademy;
+
+import java.util.HashMap;
+
+public class Homework {
+  public static void main(String[] args) {
+
+//    Create 5 trees
+//    Store the data of them in variables in your program
+//    for every tree the program should store its'
+//    type
+//    leaf color
+//    age
+//        sex
+//    you can use just variables, or lists and/or maps
+
+    HashMap<String, Object> oak = new HashMap<String, Object>();
+    oak.put("type", "Oak");
+    oak.put("leafColor", "green");
+    oak.put("age", "30");
+    oak.put("sex", "male");
+
+    HashMap<String, Object> blackBirch = new HashMap<String, Object>();
+    blackBirch.put("type", "Black Birch");
+    blackBirch.put("leafColor", "black");
+    blackBirch.put("age", "80");
+    blackBirch.put("sex", "female");
+
+    HashMap<String, Object> whiteAsh = new HashMap<String, Object>();
+    whiteAsh.put("type", "White Ash");
+    whiteAsh.put("leafColor", "white");
+    whiteAsh.put("age", "33");
+    whiteAsh.put("sex", "male");
+
+    HashMap<String, Object> bassWood = new HashMap<String, Object>();
+    bassWood.put("type", "Bass Wood");
+    bassWood.put("leafColor", "red");
+    bassWood.put("age", "32");
+    bassWood.put("sex", "female");
+
+    HashMap<String, Object> bigToothaspen = new HashMap<String, Object>();
+    bigToothaspen.put("type", "Big Tooth Aspen");
+    bigToothaspen.put("leafColor", "white");
+    bigToothaspen.put("age", "333");
+    bigToothaspen.put("sex", "male");
+
+
+
+
+
+
+
+  }
+}
