0cbca566f3786a8a0e86cdcb4d2dc864677b8a9d
Peter Antal <wrkpeter@gmail.com>
2018-08-15 13:04:55 +0200

    postIt init

---
+++ week-03/day-3/PostIt/src/com/greenfoxacademy/Main.java
@@ -0,0 +1,27 @@
+package com.greenfoxacademy;
+
+public class Main {
+  public static void main(String[] args) {
+
+//    Create a PostIt class that has
+//        a backgroundColor
+//        a text on it
+//        a textColor
+//        Create a few example post-it objects:
+//    an orange with blue text: "Idea 1"
+//    a pink with black text: "Awesome"
+//    a yellow with green text: "Superb!"
+    PostIt orange = new PostIt();
+    PostIt pink = new PostIt();
+    PostIt yellow = new PostIt();
+    orange.PostIt("red", "Awesome", "black");
+    pink.PostIt("pink", "Nothing here", "black");
+    yellow.PostIt("Yellow", "What the fuck", "black");
+
+    System.out.println();
+
+
+
+
+  }
+}
---
+++ week-03/day-3/PostIt/src/com/greenfoxacademy/PostIt.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy;
+
+public class PostIt {
+  private String backgroundColor;
+  private String text;
+  private String textColor;
+
+  public void PostIt(String backgroundColor, String text, String textColor) {
+    this.backgroundColor = backgroundColor;
+    this.textColor = text;
+    this.textColor = textColor;
+  }
+
+}
