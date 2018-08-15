6f811ebb4df2366f439c6faf84c2c9aa3ae37b34
Peter Antal <wrkpeter@gmail.com>
2018-08-15 17:11:20 +0200

    FleetOfThings init

---
+++ week-03/day-3/FleetOfThings/src/com/greenfoxacademy/FleetOfThings.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy;
+
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+
+        System.out.println(fleet);
+    }
+}
