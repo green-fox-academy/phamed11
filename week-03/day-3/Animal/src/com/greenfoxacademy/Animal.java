e24ac2e09c3a07c42a28cb2e1a3571bdea3a0dbb
Peter Antal <wrkpeter@gmail.com>
2018-08-15 17:28:02 +0200

    Animal init

---
+++ week-03/day-3/Animal/src/com/greenfoxacademy/Animal.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy;
+
+public class Animal {
+
+    public static void main(String[] args) {
+//	      Create an Animal class
+//        Every animal has a hunger value, which is a whole number
+//                Every animal has a thirst value, which is a whole number
+//                when creating a new animal object these values are created with the default 50 value
+//        Every animal can eat() which decreases their hunger by one
+//        Every animal can drink() which decreases their thirst by one
+//        Every animal can play() which increases both by one
+
+
+    }
+}
