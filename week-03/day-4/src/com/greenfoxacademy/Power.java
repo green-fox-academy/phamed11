134ced624496644963eb6a0234aec4c366fc603c
Peter Antal <wrkpeter@gmail.com>
2018-08-16 11:39:41 +0200

    Power init

---
+++ week-03/day-4/src/com/greenfoxacademy/Power.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy;
+
+public class Power {
+  public static void main(String[] args) {
+    // Given base and n that are both 1 or more, compute recursively (no loops)
+    // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+    
+  }
+}
