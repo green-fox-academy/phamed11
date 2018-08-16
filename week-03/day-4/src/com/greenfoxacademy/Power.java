f4e1b45e7f0770f57752d4b3bf5fe639f608ee16
Peter Antal <wrkpeter@gmail.com>
2018-08-16 12:35:28 +0200

    power

--- week-03/day-4/src/com/greenfoxacademy/Power.java
+++ week-03/day-4/src/com/greenfoxacademy/Power.java
@@ -4,6 +4,18 @@ public class Power {
   public static void main(String[] args) {
     // Given base and n that are both 1 or more, compute recursively (no loops)
     // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
-    
+    int a = 2;
+    int b = 3;
+    System.out.println(power(a, b));
+
+
+  }
+
+  public static int power(int a, int b) {
+    if (b == 0) {
+      return 1;
+    } else {
+      return a * power(a, b - 1);
+    }
   }
 }
