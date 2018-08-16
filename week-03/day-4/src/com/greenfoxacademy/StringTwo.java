462b3aab83e6160c6fbf7ae22c9bedf8def14ea2
Peter Antal <wrkpeter@gmail.com>
2018-08-16 14:04:00 +0200

    strings2

--- week-03/day-4/src/com/greenfoxacademy/StringTwo.java
+++ week-03/day-4/src/com/greenfoxacademy/StringTwo.java
@@ -2,6 +2,25 @@ package com.greenfoxacademy;
 
 public class StringTwo {
   public static void main(String[] args) {
-    
+    // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+    String zero = "matrixmatrix";
+    System.out.println(stringsagain(zero));
+  }
+
+  public static String stringsagain(String input) {
+    input = input.toLowerCase();
+    String result = "";
+    if (input.length() == 0) {
+      return input;
+    } else {
+      for (int i = 0; i < input.length(); i++) {
+        if (input.charAt(i) == 'x') {
+          result += input.replace();
+        } else {
+          result += input.charAt(i);
+        }
+      }
+    }
+    return result;
   }
 }
