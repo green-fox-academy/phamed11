26eb7c22b3fdd0a8daa516f3a18a3c5662c7dd7f
Peter Antal <wrkpeter@gmail.com>
2018-08-16 13:38:36 +0200

    string finished

--- week-03/day-4/src/com/greenfoxacademy/Strings.java
+++ week-03/day-4/src/com/greenfoxacademy/Strings.java
@@ -4,7 +4,26 @@ public class Strings {
   public static void main(String[] args) {
     // Given a string, compute recursively (no loops) a new string where all the
     // lowercase 'x' chars have been changed to 'y' chars.
-    
+    String test = "matrix is the BEST MATRIX even if X is not a matrix";
+    System.out.println(strings(test));
+
+
   }
 
+  public static String strings(String input) {
+    input = input.toLowerCase();
+    String result = "";
+    if (input.length() == 0) {
+      return input;
+    } else {
+      for (int i = 0; i < input.length(); i++) {
+        if (input.charAt(i) == 'x') {
+          result += 'y';
+        } else {
+          result += input.charAt(i);
+        }
+      }
+    }
+    return result;
+  }
 }
