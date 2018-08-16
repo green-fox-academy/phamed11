a1828455f52d7b05172906e765749d2995901038
Peter Antal <wrkpeter@gmail.com>
2018-08-16 11:38:28 +0200

    sumDigit fini

--- week-03/day-4/src/com/greenfoxacademy/SumDigit.java
+++ week-03/day-4/src/com/greenfoxacademy/SumDigit.java
@@ -5,9 +5,13 @@ public class SumDigit {
     // Given a non-negative int n, return the sum of its digits recursively (no loops).
     // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
     // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
-
+    System.out.println(sumDigit(235553));
   }
   public static int sumDigit(int n) {
-    
+    if (n == 0 ) {
+    return 0;
+    } else {
+      return (n % 10) + sumDigit(n / 10);
+    }
   }
 }
