ec0c0d58303275e4f5caaaa91427aff4dc334323
Peter Antal <wrkpeter@gmail.com>
2018-08-17 10:42:39 +0200

    petrolstation ini

---
+++ week-03/day-3/PetrolStation/src/com/greenfoxacademy/Main.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Create Station and Car classes
+////            Station
+////        gasAmount
+////        refill(car) -> decreases the gasAmount by the capacity of the car and increases the cars gasAmount
+////        Car
+////            gasAmount
+////        capacity
+////        create constructor for Car where:
+////        initialize gasAmount -> 0
+////        initialize capacity -> 100
+
+    }
+}
