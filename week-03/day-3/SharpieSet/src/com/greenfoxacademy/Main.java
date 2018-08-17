f5ed0562c77f701073e67bc28e35bf22cec822ed
Peter Antal <wrkpeter@gmail.com>
2018-08-17 13:37:13 +0200

    sharpieset finished

--- week-03/day-3/SharpieSet/src/com/greenfoxacademy/Main.java
+++ week-03/day-3/SharpieSet/src/com/greenfoxacademy/Main.java
@@ -1,5 +1,8 @@
 package com.greenfoxacademy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,7 +11,13 @@ public class Main {
 //        it contains a list of Sharpie
 //                countUsable() -> sharpie is usable if it has ink in it
 //        removeTrash() -> removes all unusable sharpies
-
+      Sharpie black = new Sharpie("Black", 44f);
+      Sharpie white = new Sharpie("White", 55f);
+      Sharpie red = new Sharpie("Red", 333f);
+      Sharpie green = new Sharpie("Green", 67f);
+      SharpieSet tolltarto = new SharpieSet();
+      tolltarto.addSharpies(black);
+      tolltarto.printList();
 
     }
 }
--- week-03/day-3/SharpieSet/src/com/greenfoxacademy/SharpieSet.java
+++ week-03/day-3/SharpieSet/src/com/greenfoxacademy/SharpieSet.java
@@ -1,4 +1,40 @@
 package com.greenfoxacademy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SharpieSet {
+ArrayList<Sharpie> list = new ArrayList<>();
+
+  public SharpieSet() {
+
+  }
+
+  public void addSharpies(Sharpie ceruza) {
+    list.add(ceruza);
+
+   }
+
+  public int countUsable() {
+    int usable = 0;
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).inkAmount > 0) {
+        usable++;
+      }
+    }
+    return usable;
+  }
+
+  public void removeTrash() {
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).inkAmount == 0) {
+        list.remove(list.get(i));
+      }
+    }
+  }
+  public void printList() {
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i).inkAmount + list.get(i).color + list.get(i).width);
+    }
+  }
 }
