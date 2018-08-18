1796a7de706d8e2ae688093bb758ba9e213ebaf3
Peter Antal <wrkpeter@gmail.com>
2018-08-18 10:59:35 +0200

    farm almost finished

--- week-03/day-3/Farm/src/com/greenfoxacademy/Farm.java
+++ week-03/day-3/Farm/src/com/greenfoxacademy/Farm.java
@@ -24,11 +24,16 @@ public class Farm {
 
   public void slaughter() {
     int[] hungerValues = new int[list.size()];
+    int[] swap = new int[hungerValues.length];
     for (int i = 0; i < list.size() - 1; i++) {
       hungerValues[i] = list.get(i).hunger;
     }
     Arrays.sort(hungerValues);
+    for (int i = 0; i < hungerValues.length; i++) {
+      swap[i] = hungerValues[hungerValues.length - 1 - i];
+    }
     list.remove(list.get(hungerValues[0]));
+    System.out.println("I am sorry " + list.get(hungerValues[0]).name + " had to die");
   }
 
 }
--- week-03/day-3/Farm/src/com/greenfoxacademy/Main.java
+++ week-03/day-3/Farm/src/com/greenfoxacademy/Main.java
@@ -13,9 +13,9 @@ public class Main {
 //        slaughter() -> removes the least hungry animal
 
     Farm myFarm = new Farm(10);
-    Animal cow = new Animal("tehen");
-    Animal sheep = new Animal("barany");
-    Animal chicken = new Animal("csirke");
+    Animal cow = new Animal("Marci the cow");
+    Animal sheep = new Animal("Gyuri the sheep");
+    Animal chicken = new Animal("Etel the chicken");
     cow.eat();
     chicken.play();
     sheep.eat();
@@ -24,27 +24,30 @@ public class Main {
     myFarm.list.add(sheep);
     myFarm.list.add(chicken);
     myFarm.list.add(cow);
-
     System.out.println();
-
-    myFarm.breed(cow, "tehen2");
-    myFarm.breed(cow, "tehen3");
-    myFarm.breed(cow, "tehen4");
-    myFarm.breed(sheep, "barany2");
+    myFarm.breed(cow, "Joe the cow");
+    myFarm.breed(cow, "Imre the cow");
+    myFarm.breed(chicken, "Koko the chicken");
+    myFarm.breed(sheep, "Bee the sheep");
     myFarm.list.get(0).eat();
     myFarm.list.get(3).eat();
     myFarm.list.get(3).eat();
     myFarm.list.get(1).eat();
     myFarm.list.get(4).eat();
-
-
+    myFarm.list.get(3).eat();
+    myFarm.list.get(3).eat();
+    myFarm.list.get(3).eat();
+    myFarm.list.get(3).eat();
 
 
     for (int i = 0; i < myFarm.list.size() ; i++) {
-      System.out.println("The " + myFarm.list.get(i).name + "'s hunger level is: " + myFarm.list.get(i).hunger);
+      System.out.println((i + 1) +  ". The " + myFarm.list.get(i).name + "'s hunger level is: " + myFarm.list.get(i).hunger);
     }
+    System.out.println();
     System.out.println("This farm has : " + myFarm.list.size() + " animals");
+    System.out.println();
     myFarm.slaughter();
+
 
     System.out.println();
     System.out.println("This farm has : " + myFarm.list.size() + " animals");
