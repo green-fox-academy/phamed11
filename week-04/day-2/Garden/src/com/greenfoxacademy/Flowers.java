07e9daec4ea480090ce0bd03001a4739cd840d07
Peter Antal <wrkpeter@gmail.com>
2018-08-21 01:04:54 +0200

    minor changes to garden

--- week-04/day-2/Garden/src/com/greenfoxacademy/Flowers.java
+++ week-04/day-2/Garden/src/com/greenfoxacademy/Flowers.java
@@ -3,12 +3,9 @@ package com.greenfoxacademy;
 import java.util.ArrayList;
 
 public class Flowers extends Garden{
-  ArrayList<Flowers> Ff = new ArrayList<>();
 
   public Flowers(String color) {
     this.color = color;
-    Flowers name = new Flowers();
-    Ff.add(name);
   }
 
   public Flowers() {
--- week-04/day-2/Garden/src/com/greenfoxacademy/Trees.java
+++ week-04/day-2/Garden/src/com/greenfoxacademy/Trees.java
@@ -3,12 +3,9 @@ package com.greenfoxacademy;
 import java.util.ArrayList;
 
 public class Trees extends Garden {
-  ArrayList<Trees> Tt = new ArrayList<>();
 
   public Trees(String color) {
     this.color = color;
-    Trees name = new Trees();
-    Tt.add(name);
   }
 
   public Trees() {
