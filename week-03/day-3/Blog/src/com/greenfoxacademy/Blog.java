2167c0e13690c8170e671f13f9eb491ce94f49d6
Peter Antal <wrkpeter@gmail.com>
2018-08-20 18:13:16 +0200

    blog started

---
+++ week-03/day-3/Blog/src/com/greenfoxacademy/Blog.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy;
+
+import java.util.ArrayList;
+
+public class Blog {
+  ArrayList<BlogPost> blog = new ArrayList<>();
+  String name;
+
+  public Blog() {
+    this.name = name;
+  }
+
+  public void addBlogPost(BlogPost name) {
+    blog.add(name);
+  }
+
+  
+}
---
+++ week-03/day-3/Blog/src/com/greenfoxacademy/Main.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Reuse your BlogPost class
+//        Create a Blog class which can
+//            store a list of BlogPosts
+//            add BlogPosts to the list
+//            delete(int) one item at given index
+//        update(int, BlogPost) one item at the given index and update it with another BlogPost
+
+    BlogPost first = new BlogPost();
+
+    }
+}
