99713f8a010ade40f78811e782cbd872f4d2cc26
Peter Antal <wrkpeter@gmail.com>
2018-08-20 21:37:49 +0200

    garden init

---
+++ week-04/day-2/Garden/src/com/greenfoxacademy/Flowers.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy;
+
+public class Flowers {
+  String color;
+  int thirstLevel;
+
+
+  public Flowers(String color, int thirstLevel) {
+    this.color = color;
+    this.thirstLevel = thirstLevel;
+  }
+
+  public Flowers() {
+
+  }
+
+
+}
---
+++ week-04/day-2/Garden/src/com/greenfoxacademy/Trees.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy;
+
+public class Trees extends Flowers{
+
+
+  public Trees(String color, int thirstLevel) {
+    this.color = color;
+    this.thirstLevel = thirstLevel;
+  }
+
+  public Trees() {
+
+  }
+  
+}
+
