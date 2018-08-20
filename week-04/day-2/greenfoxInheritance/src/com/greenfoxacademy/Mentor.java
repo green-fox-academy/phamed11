3bb01d3fd8f799f51806dc3626229a0158404aa7
Peter Antal <wrkpeter@gmail.com>
2018-08-20 21:01:50 +0200

    greenfox inheritance added sponsor

---
+++ week-04/day-2/greenfoxInheritance/src/com/greenfoxacademy/Mentor.java
@@ -0,0 +1,30 @@
+package com.greenfoxacademy;
+
+public class Mentor extends Person {
+  String level;
+
+  public Mentor(String name, int age, String gender, String level) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.level = level;
+  }
+
+  public Mentor() {
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+    level = "intermediate";
+  }
+
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " " + level + " mentor.");
+  }
+
+
+}
+
