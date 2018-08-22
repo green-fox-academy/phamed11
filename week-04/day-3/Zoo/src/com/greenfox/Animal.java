94a10a698de339d5b5014c8e893938109d37c394
Peter Antal <wrkpeter@gmail.com>
2018-08-22 18:54:26 +0200

    Animals init again

--- week-04/day-3/Zoo/src/com/greenfox/Animal.java
+++ week-04/day-3/Zoo/src/com/greenfox/Animal.java
@@ -3,6 +3,10 @@ package com.greenfox;
 public abstract class Animal {
   String name;
   int age;
+  String gender;
+  String favouriteFood;
+  int numberOfLegs;
+  String soundsLike;
 
   public String getName() {
     return name;
---
+++ week-04/day-3/Zoo/src/com/greenfox/Mammal.java
@@ -0,0 +1,4 @@
+package com.greenfox;
+
+public class Mammal extends Animal {
+}
