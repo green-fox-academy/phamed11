f04761f4779021caedc91d4bbb77b88cdebf5a5d
Peter Antal <wrkpeter@gmail.com>
2018-08-22 17:46:41 +0200

    zoo init

---
+++ week-04/day-3/Zoo/src/com/greenfox/Animal.java
@@ -0,0 +1,14 @@
+package com.greenfox;
+
+public abstract class Animal {
+  String name;
+  int age;
+
+  public String getName() {
+    return name;
+  }
+
+  public void breed() {
+    
+  }
+}
---
+++ week-04/day-3/Zoo/src/com/greenfox/main.java
@@ -0,0 +1,4 @@
+package com.greenfox;
+
+public class main {
+}
