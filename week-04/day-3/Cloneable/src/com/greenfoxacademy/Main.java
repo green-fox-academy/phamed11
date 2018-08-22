ce0902820109a7e8783b49c3fd6d71a16f11e2df
Peter Antal <wrkpeter@gmail.com>
2018-08-22 13:10:14 +0200

    cloneable init

---
+++ week-04/day-3/Cloneable/src/com/greenfoxacademy/Main.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+
+  }
+}
---
+++ week-04/day-3/Cloneable/src/com/greenfoxacademy/Student.java
@@ -0,0 +1,33 @@
+package com.greenfoxacademy;
+
+public class Student extends Person {
+  String previousOrganization;
+  int skippedDays;
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    skippedDays = 0;
+  }
+
+  public Student() {
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + ". From " + previousOrganization
+        + " who skipped " + skippedDays + " days from the course already.");
+  }
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  public int skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+    return skippedDays;
+  }
+
+
+}
