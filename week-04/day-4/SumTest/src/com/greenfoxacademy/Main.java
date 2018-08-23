8174187dc1c060506deccc902963df779d2414a1
Peter Antal <wrkpeter@gmail.com>
2018-08-23 15:37:23 +0200

    sumTest progress

--- week-04/day-4/SumTest/src/com/greenfoxacademy/Main.java
+++ week-04/day-4/SumTest/src/com/greenfoxacademy/Main.java
@@ -4,15 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
-
-      ArrayList<Integer> test = new ArrayList<>(Arrays.asList(44, 22));
-
-      Sum testsum = new Sum();
-
-      System.out.println(testsum.sum(test));
-
-
-    }
+  }
 }
--- week-04/day-4/SumTest/src/com/greenfoxacademy/SumTest.java
+++ week-04/day-4/SumTest/src/com/greenfoxacademy/SumTest.java
@@ -2,15 +2,19 @@ package com.greenfoxacademy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class SumTest {
 
   @Test
-  public void sayIfEqual() {
-
+  public void sum() {
+    SumTest basic = new SumTest();
+    ArrayList<Integer> testList = new ArrayList<>(Arrays.asList(11, 33));
+    assertArrayEquals(44, basic.sum(testList));
   }
-
 
 
 }
