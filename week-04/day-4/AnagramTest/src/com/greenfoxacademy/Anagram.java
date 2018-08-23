d54d28257f0118a031781990fbc329a7aa6b0f75
Peter Antal <wrkpeter@gmail.com>
2018-08-23 16:14:48 +0200

    anagram test created

--- week-04/day-4/AnagramTest/src/com/greenfoxacademy/Anagram.java
+++ week-04/day-4/AnagramTest/src/com/greenfoxacademy/Anagram.java
@@ -1,5 +1,35 @@
 package com.greenfoxacademy;
 
-public class Anagram {
+import java.util.Arrays;
 
+public class Anagram {
+  public static void main(String[] args) {
+    String first = "doguoij";
+    String second = "jiougod";
+    Anagram(first,second);
+
+
+  }
+
+  public static void Anagram(String first, String second) {
+    String[] splitfirst = {""};
+    String[] splitsecond = {""};
+    String toCheck1 = "";
+    String toCheck2 = "";
+    splitfirst = first.split("");
+    splitsecond = second.split("");
+    Arrays.sort(splitfirst);
+    Arrays.sort(splitsecond);
+    for (String s : splitfirst) {
+      toCheck1 += s;
+    }
+    for (String d : splitsecond) {
+      toCheck2 += d;
+    }
+    if (toCheck1.equals(toCheck2)) {
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
+  }
 }
---
+++ week-04/day-4/AnagramTest/test/com/greenfoxacademy/AnagramTest.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy;
+
+import static org.junit.Assert.*;
+
+public class AnagramTest {
+  
+
+}
