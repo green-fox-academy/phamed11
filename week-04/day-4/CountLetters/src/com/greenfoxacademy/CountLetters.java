27915126b0eeb7d9d0e69b1e82c50501ab24068e
Peter Antal <wrkpeter@gmail.com>
2018-08-23 16:53:40 +0200

    count letter progress

--- week-04/day-4/CountLetters/src/com/greenfoxacademy/CountLetters.java
+++ week-04/day-4/CountLetters/src/com/greenfoxacademy/CountLetters.java
@@ -1,4 +1,12 @@
 package com.greenfoxacademy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountLetters {
+
+  public Map<String, Integer> countLetters(String test) {
+    Map<String, Integer> dictionary = new HashMap<>();
+    
+  }
 }
