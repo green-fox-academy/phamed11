64618609336ed95de90f3f0aa854bacdeea7d7b8
Peter Antal <wrkpeter@gmail.com>
2018-08-28 17:11:27 +0200

    lottery done

--- week-03/day-2/src/com/greenfoxacademy/Lottery.java
+++ week-03/day-2/src/com/greenfoxacademy/Lottery.java
@@ -1,13 +1,11 @@
 package com.greenfoxacademy;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Lottery {
 
@@ -41,12 +39,47 @@ public class Lottery {
         allTheNums.put(lotteryNumbers.get(i), allTheNums.get(lotteryNumbers.get(i)) + 1);
       }
     }
-    int biggest = 0;
-    for (String eachValue : allTheNums.keySet()) {
-      if (allTheNums.get(eachValue) > biggest) {
-        biggest = allTheNums.get(eachValue);
+    List<Integer> list = new ArrayList<Integer>(allTheNums.values());
+    Collections.sort(list, Collections.reverseOrder());
+    List<Integer> top5 = list.subList(0, 5);
+    List<String> mostFrequentNumbers = new ArrayList<>();
+//    for (String eachValue : allTheNums.keySet()) {
+//      if (allTheNums.get(eachValue) == top5.get(0)) {
+//        mostFrequentNumbers.add(eachValue);
+//      }
+//    }
+//    for (String eachValue : allTheNums.keySet()) {
+//      if (allTheNums.get(eachValue) == top5.get(1)) {
+//        mostFrequentNumbers.add(eachValue);
+//      }
+//    }
+//    for (String eachValue : allTheNums.keySet()) {
+//      if (allTheNums.get(eachValue) == top5.get(2)) {
+//        mostFrequentNumbers.add(eachValue);
+//      }
+//    }
+//    for (String eachValue : allTheNums.keySet()) {
+//      if (allTheNums.get(eachValue) == top5.get(3)) {
+//        mostFrequentNumbers.add(eachValue);
+//      }
+//    }
+//    for (String eachValue : allTheNums.keySet()) {
+//      if (allTheNums.get(eachValue) == top5.get(4)) {
+//        mostFrequentNumbers.add(eachValue);
+//      }
+//    }
+    for (int i = 0; i < 5; i++) {
+      for (String eachKey : allTheNums.keySet()) {
+        if (allTheNums.get(eachKey) == top5.get(i)) {
+          mostFrequentNumbers.add(eachKey);
+        }
       }
     }
-    System.out.println(biggest);
+    System.out.println(top5);
+    System.out.println(mostFrequentNumbers);
   }
 }
+
+
+
+
