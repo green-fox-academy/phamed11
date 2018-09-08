f459804f43d92a78f4f7333d3c32f170a7a6e20d
Peter Antal <wrkpeter@gmail.com>
2018-09-08 21:06:28 +0200

    feat(Excersises) 2 done

--- week-06/day-1/src/Exercise1.java
+++ week-06/day-1/src/Exercise1.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise1 {
   public static void main(String[] args) {
@@ -9,7 +11,7 @@ public class Exercise1 {
 //    ```java
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
 
-    numbers.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
-
+    List<Integer> evenNumbers = numbers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+    System.out.println(evenNumbers);
   }
 }
--- week-06/day-1/src/Exercise2.java
+++ week-06/day-1/src/Exercise2.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Exercise2 {
   public static void main(String[] args) {
-    
+//    # Exercise 2
+//    Write a Stream Expression to get the average value of the odd numbers from the following array:
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+    double average = numbers.stream()
+        .filter(x -> x % 2 != 0)
+        .mapToInt(x -> x)
+        .average()
+        .getAsDouble();
+
+    System.out.println(average);
+    System.out.println(aver(numbers));
+  }
+
+  public static double aver(ArrayList<Integer> list) {
+    int sum = 0;
+    int numberOfOdds = 0;
+    for (Integer numb : list) {
+      if (numb % 2 != 0) {
+        sum += numb;
+        numberOfOdds++;
+      }
+    }
+    return (double) sum / numberOfOdds;
   }
 }
