b0b35f9fab09c72d33002fc053844dda3a74669b
Peter Antal <wrkpeter@gmail.com>
2018-09-08 21:18:30 +0200

    feat(streams):exercise3 finished

---
+++ week-06/day-1/src/Exercise3.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise3 {
+  public static void main(String[] args) {
+//# Exercise 3
+//    Write a Stream Expression to get the squared value of the positive numbers from the following array:
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+    List<Integer> squared =
+        numbers.stream()
+        .filter(x -> x > 0)
+        .map(x -> x * x)
+        .collect(Collectors.toList());
+
+    System.out.println(squared);
+  }
+}
