f17c1227aee2c26ad4f98921ebd2582c9e342e7c
Peter Antal <wrkpeter@gmail.com>
2018-09-10 14:27:06 +0200

    feat(Stream): exercise10 done

--- week-06/day-1/src/Exercise10.java
+++ week-06/day-1/src/Exercise10.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Exercise10 {
   public static void main(String[] args) {
 
@@ -12,7 +16,49 @@ public class Exercise10 {
     Fox five = new Fox("Albino", "whitefox", "white");
     Fox six = new Fox("Jesus", "bluefox", "blue");
 
-    
+    ArrayList<Fox> theHound = new ArrayList<>();
+    theHound.add(one);
+    theHound.add(two);
+    theHound.add(three);
+    theHound.add(four);
+    theHound.add(five);
+    theHound.add(six);
 
+    List<String> onlyGreen = theHound.stream()
+        .filter(fox -> fox.getColor().equals("green"))
+        .map(Fox::getName)
+        .collect(Collectors.toList());
+
+    List<String> pallidaAndGreen = theHound.stream()
+        .filter(fox -> fox.getColor().equals("green") && fox.getType().equals("pallinda"))
+        .map(Fox::getName)
+        .collect(Collectors.toList());
+
+
+    System.out.println(onlyGreen);
+    System.out.println(pallidaAndGreen);
+    System.out.println("\nThis is the method result: ");
+    System.out.println(onlyGreenMethod(theHound));
+    System.out.println(onlyPallindaMethod(theHound));
+  }
+
+  public static ArrayList<String> onlyGreenMethod(ArrayList<Fox> list) {
+    ArrayList<String> result = new ArrayList<>();
+    for (Fox fox : list) {
+      if (fox.getColor().equals("green")) {
+        result.add(fox.getName());
+      }
+    }
+    return result;
+  }
+
+  public static ArrayList<String> onlyPallindaMethod(ArrayList<Fox> list) {
+    ArrayList<String> result = new ArrayList<>();
+    for (Fox fox : list) {
+      if (fox.getColor().equals("green") && fox.getType().equals("pallinda")) {
+        result.add(fox.getName());
+      }
+    }
+    return result;
   }
 }
