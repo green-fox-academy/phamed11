b8a2939e9b2e95e01cf7b7d2d08b6f9741dcdc09
Peter Antal <wrkpeter@gmail.com>
2018-09-17 15:30:03 +0200

    feat(spring): name added

--- week-07/day-1/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloRESTController.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloRESTController.java
@@ -3,14 +3,16 @@ package com.greenfoxacademy.springstart.Controllers;
 
 import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
+
   @RequestMapping("/greeting")
-  public Greeting greeting() {
-    Greeting greeting = new Greeting(1, "Hello World");
+  public Greeting greeting(@RequestParam(value = "name", required = true) String name) {
+    Greeting greeting = new Greeting(1, "Hello " + name);
     return greeting;
   }
 
