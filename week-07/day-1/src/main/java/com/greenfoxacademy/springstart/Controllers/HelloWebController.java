54e75d44994034047ba04d18eed5491a52fefbdc
Peter Antal <wrkpeter@gmail.com>
2018-09-17 17:38:40 +0200

    feat(spring): world counter done

--- week-07/day-1/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebController.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HelloWebController {
+  AtomicLong number = new AtomicLong(1);
 
 //  @RequestMapping(value = "/web/greeting", method = RequestMethod.GET)
 //  public String greeting(Model model) {
@@ -16,9 +19,16 @@ public class HelloWebController {
 //    return "greeting";
 //  }
 
+//  @GetMapping("/web/greeting")
+//  public String greeting(Model model) {
+//    model.addAttribute("name", " World");
+//    return "greeting";
+//  }
+
   @GetMapping("/web/greeting")
   public String greeting(Model model) {
-    model.addAttribute("name", " World");
+    model.addAttribute("name", " Peter");
+    model.addAttribute("number", number.getAndIncrement());
     return "greeting";
   }
 }
