db2c4f5768586c6cca9d51e496e604bbd8838d9e
Peter Antal <wrkpeter@gmail.com>
2018-09-18 13:47:08 +0200

    (bankofsimba): html done

--- week-07/day-2/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Controllers/AccountController.java
+++ week-07/day-2/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Controllers/AccountController.java
@@ -2,14 +2,19 @@ package com.greenfoxacademy.bankofsimba.Controllers;
 
 import com.greenfoxacademy.bankofsimba.Model.BankAccount;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
 
+  BankAccount bankAccount = new BankAccount("Simba", "2000", "lion");
+
   @GetMapping("/show")
-  public String showsBankAccount() {
-    BankAccount bankAccount = new BankAccount("Simba", "2000", "lion");
+  public String showsBankAccount(Model model) {
+    model.addAttribute("name", bankAccount.getName());
+    model.addAttribute("balance", bankAccount.getBalance());
+    model.addAttribute("animalType", bankAccount.getAnimalType());
     return "show";
   }
 
--- week-07/day-2/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Model/BankAccount.java
+++ week-07/day-2/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Model/BankAccount.java
@@ -8,9 +8,35 @@ public class BankAccount {
   public BankAccount() {
   }
 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBalance() {
+    return balance;
+  }
+
+  public void setBalance(String balance) {
+    this.balance = balance;
+  }
+
+  public String getAnimalType() {
+    return animalType;
+  }
+
+  public void setAnimalType(String animalType) {
+    this.animalType = animalType;
+  }
+
   public BankAccount(String name, String balance, String animalType) {
     this.name = name;
     this.balance = balance;
     this.animalType = animalType;
+
+
   }
 }
