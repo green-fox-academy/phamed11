2deadecb397bd9b5e90eb79e31213fdd2d7792bc
Peter Antal <wrkpeter@gmail.com>
2018-09-19 12:53:14 +0200

    (hellobeanworld): init

---
+++ week-07/day-3/HelloBeanWorld/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.hellobeanworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobeanworldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellobeanworldApplication.class, args);
+  }
+}
