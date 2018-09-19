28d9ca39dc3a0798cc0df046d2bf960f60a90fea
Peter Antal <wrkpeter@gmail.com>
2018-09-19 13:04:45 +0200

    (hellobeanworld): done

--- week-07/day-3/HelloBeanWorld/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
+++ week-07/day-3/HelloBeanWorld/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
@@ -1,12 +1,22 @@
 package com.greenfoxacademy.hellobeanworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellobeanworldApplication {
+public class HellobeanworldApplication implements CommandLineRunner {
+  @Autowired
+  Printer printer;
 
   public static void main(String[] args) {
     SpringApplication.run(HellobeanworldApplication.class, args);
   }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("Hello");
+  }
+
 }
