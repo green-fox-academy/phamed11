fa1ce96b9a8da397bd4430a336622ac2eb9ef4fb
Peter Antal <wrkpeter@gmail.com>
2018-09-20 11:22:01 +0200

    feat(FoxTamagochi): redirect done

--- week-07/day-4/FoxTamagochi/src/main/java/com/greenfoxacademy/foxtamagochi/Modells/Fox.java
+++ week-07/day-4/FoxTamagochi/src/main/java/com/greenfoxacademy/foxtamagochi/Modells/Fox.java
@@ -6,4 +6,36 @@ public class Fox {
   private String food;
   private String drink;
 
+  public Fox() {
+  }
+
+  public Fox(String name, String food, String drink) {
+    this.name = name;
+    this.food = food;
+    this.drink = drink;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public void setFood(String food) {
+    this.food = food;
+  }
+
+  public String getDrink() {
+    return drink;
+  }
+
+  public void setDrink(String drink) {
+    this.drink = drink;
+  }
 }
