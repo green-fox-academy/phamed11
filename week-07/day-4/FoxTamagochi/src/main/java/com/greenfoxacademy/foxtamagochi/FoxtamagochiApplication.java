9a26a92e37f5d07be88b2e00c2aa287b704a97eb
Peter Antal <wrkpeter@gmail.com>
2018-09-20 09:53:12 +0200

    feat(FoxTamagochi): project init

---
+++ week-07/day-4/FoxTamagochi/src/main/java/com/greenfoxacademy/foxtamagochi/FoxtamagochiApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.foxtamagochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxtamagochiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FoxtamagochiApplication.class, args);
+  }
+}
