ec8bc2a09d180813827491a14a5352662920d83e
Peter Antal <wrkpeter@gmail.com>
2018-09-21 10:35:51 +0200

    feat(FoxTamagochi): nutrition page ready working as for now

--- week-07/day-4/FoxTamagochi/src/main/java/com/greenfoxacademy/foxtamagochi/Services/FoxService.java
+++ week-07/day-4/FoxTamagochi/src/main/java/com/greenfoxacademy/foxtamagochi/Services/FoxService.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.foxtamagochi.Services;
 
 import com.greenfoxacademy.foxtamagochi.Modells.Fox;
-import com.greenfoxacademy.foxtamagochi.Repositories.FoxHorde;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public interface FoxService {
+
+
+
 
 
 
--- week-07/day-4/FoxTamagochi/src/main/java/com/greenfoxacademy/foxtamagochi/Services/FoxServiceImpl.java
+++ week-07/day-4/FoxTamagochi/src/main/java/com/greenfoxacademy/foxtamagochi/Services/FoxServiceImpl.java
@@ -1,13 +1,6 @@
 package com.greenfoxacademy.foxtamagochi.Services;
 
-import com.greenfoxacademy.foxtamagochi.Modells.Fox;
-import com.greenfoxacademy.foxtamagochi.Repositories.FoxHorde;
-import org.springframework.beans.factory.annotation.Autowired;
 
-public class FoxServiceImpl implements FoxService {
-  FoxHorde foxHorde;
-
-
-
+public class FoxServiceImpl implements FoxService{
 
 }
