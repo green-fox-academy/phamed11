8f0279bbccab180bf77b55a0547fd7821edb0fb3
Peter Antal <wrkpeter@gmail.com>
2018-09-25 12:42:41 +0200

    feat(listingtodosh2): init

---
+++ week-08/day-2/ListingTodosH2/src/main/java/com/greenfoxacademy/listingtodosh2/Listingtodosh2Application.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.listingtodosh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Listingtodosh2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Listingtodosh2Application.class, args);
+  }
+}
