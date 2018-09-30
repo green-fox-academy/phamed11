0fe2374ba2c13cbe511b5bc22c2756f69b18a79a
Peter Antal <wrkpeter@gmail.com>
2018-09-30 14:45:13 +0200

    feat(redditclone): init

---
+++ week-08/day-5/RedditClone/src/main/java/com/greenfoxacademy/redditclone/RedditcloneApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.redditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditcloneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RedditcloneApplication.class, args);
+  }
+}
