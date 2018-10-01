531634b11d4e9fc70cc1bd885d0407cfce4f5417
Peter Antal <wrkpeter@gmail.com>
2018-10-01 13:05:25 +0200

    feat(json exercises): init

---
+++ week-09/day-1/JsonExercises/src/main/java/com/greenfoxacademy/jsonexercises/JsonexercisesApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.jsonexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonexercisesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JsonexercisesApplication.class, args);
+  }
+}
---
+++ week-09/day-1/JsonExercises/src/test/java/com/greenfoxacademy/jsonexercises/JsonexercisesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.jsonexercises;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsonexercisesApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
