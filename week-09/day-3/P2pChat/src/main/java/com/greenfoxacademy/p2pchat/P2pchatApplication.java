297238e450e0221ea1a875a3c2720f07fd175bde
Peter Antal <wrkpeter@gmail.com>
2018-10-03 10:50:52 +0200

    feat(p2pchat): init

---
+++ week-09/day-3/P2pChat/src/main/java/com/greenfoxacademy/p2pchat/P2pchatApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.p2pchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pchatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(P2pchatApplication.class, args);
+  }
+}
---
+++ week-09/day-3/P2pChat/src/test/java/com/greenfoxacademy/p2pchat/P2pchatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.p2pchat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class P2pchatApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
